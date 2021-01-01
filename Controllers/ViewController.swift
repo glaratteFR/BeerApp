a49a2a561cbdbf01b39288dd3eb3b4bc356b1dc1
Jorhe <jorgeperezramos@usal.es>
2021-01-01 10:55:23 +0100

    Add and delete functionalities

--- Controllers/ViewController.swift
+++ Controllers/ViewController.swift
@@ -10,12 +10,124 @@ import UIKit
 
 class ViewController: UITableViewController {
 
+    
+    @IBOutlet weak var addBeerButt: UIButton!
+    @IBOutlet weak var delBeerButt: UIButton!
+    
     var model = Model()
+    var editingStyle:UITableViewCell.EditingStyle?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.appEnterBackGround(notification:)), name: UIApplication.didEnterBackgroundNotification, object: nil)//When app goes background/ends saves data
+        
+        self.editingStyle = UITableViewCell.EditingStyle.none//Look up style
+        print("%Editing style  None")
         tableView.reloadData()
         
+        
             
+    }//end view did load
+    @objc func appEnterBackGround(notification: NSNotification){
+           print("%App passed to background")
+           if !model.writeProducersInfosToDocuments(model.NAME_OF_PRODUCERS_FILE_IN_DOCUMENTS, folder: model.NAME_OF_FOLDER_IN_DOCUMENTS){
+               notifyUser(self, alertTitle: "IO Error", alertMessage: "Error When Writting Producers", runOnOK: {_ in})
+               
+           }else{
+               print("%Producers Saved Correctly")
+               
+           }
+            
+       }
+    
+    @IBAction func returnfromDetail(segue:UIStoryboardSegue)->Void{//Method for returning from Segues
+           model.producers.forEach{ b in b.beersCollect?.sort(by: {($0.nameBeer) < ($1.nameBeer)})}
+           tableView.reloadData()
+       }
+       
+       
+       //Functions for buttons
+       @IBAction func addBeerAct(_ sender: Any){
+           print("             #Vamos a añadir una cerveza")
+           if self.editingStyle == UITableViewCell.EditingStyle.none{
+               print("             #Estilo de edit activado")
+               print("%Number producers --> \(model.producers.count)")
+                   self.editingStyle = .insert
+                   addBeerButt.setTitle("Done", for: .normal)//¿?¿ Posible error
+                   delBeerButt.isEnabled = false
+                   tableView.setEditing(true, animated: true)
+                   
+           }else if self.editingStyle == .insert{
+                   
+                   self.editingStyle = UITableViewCell.EditingStyle.none
+                   delBeerButt.isEnabled = true
+                   addBeerButt.setTitle("Add Beer", for: .normal)//¿?¿ Posible error
+                   tableView.setEditing(false, animated: true)
+                   
+               }
+           
+       }
+       
+       
+       @IBAction func delBeerAct(_ sender: Any){
+           
+           if self.editingStyle == UITableViewCell.EditingStyle.none{
+               
+               
+                   self.editingStyle = .delete
+                   delBeerButt.setTitle("Done", for: .normal)//¿?¿ Posible error
+                   addBeerButt.isEnabled = false
+                   tableView.setEditing(true, animated: true)
+                   
+           }else if self.editingStyle == .delete{
+                   
+                   self.editingStyle = UITableViewCell.EditingStyle.none
+                   addBeerButt.isEnabled = true
+                   delBeerButt.setTitle("Remove Beer", for: .normal)//¿?¿ Posible error
+                   tableView.setEditing(false, animated: true)
+                   
+               }
+           
+       }
+    
+    //Return for a given moment the sate of table (editing, none...)
+    override func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle{
+        //return self.editingStyle ?? .none
+        return self.editingStyle!
+        
+
+    }
+    
+    
+    //The producer affected by deletion or addition
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath){
+        let section = indexPath.section
+        let row = indexPath.row
+        let producer = model.producers[section]
+        
+        switch self.editingStyle! {
+        case .none:
+            break
+        case .insert:
+            let b = Beer()
+            b.producerBeer = producer.nameProducer
+            
+            //add to porducer list
+            producer.beersCollect?.insert(b, at: row + 1)
+            producer.beersCollect?.sort(by: {($0.nameBeer) < ($1.nameBeer)})
+            
+            //add to general list
+            tableView.insertRows(at: [indexPath], with: .fade)
+            tableView.reloadSections(IndexSet(integer: section), with: .fade)
+        case .delete:
+            producer.beersCollect?.remove(at: row)
+            tableView.deleteRows(at: [indexPath], with:.fade)
+            tableView.reloadData()
+        @unknown default:
+            break
+        }
+        
     }
     
     //
