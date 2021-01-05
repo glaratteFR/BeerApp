9d46589a9f2ebb1753daf23f11258bf5922552a6
Jorhe <jorgeperezramos@usal.es>
2021-01-05 19:53:20 +0100

    Solved bug  where the beers won't show up on tableView

--- Controllers/ViewController.swift
+++ Controllers/ViewController.swift
@@ -19,19 +19,51 @@ class ViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("6666666666666")
+        print("-----------------------------")
+        print(model.producersNamed)
+        print(model.producersNamed.forEach{$0.value.beersCollect?.forEach{print("                        BEERS IN producersNAmed --> \($0.nameBeer)")}})
+       
+        print(model.producersNamed.first?.value.beersCollect?.count)
+  
+        print(model.producers.count)
+
+   
+        model.producers = model.producersNamed.map { (name, producer) in
+            return producer
+        }
+        print(model.producers[0].beersCollect?.count)
+       print(model.producers[0].beersCollect?[0].nameBeer)
+        print(model.producers[0].beersCollect?[1].nameBeer)
+        print(model.producers[0].beersCollect?[2].nameBeer)
+        
+        
+ 
+        print("-----------------------------")
+        print(model.producersNamed.forEach{print($0)})
+        model.producers[0].beersCollect?.forEach{print("                        BEERS IN PRODUCERS --> \($0.nameBeer)")}
+       
        
         NotificationCenter.default.addObserver(self, selector: #selector(ViewController.appEnterBackGround(notification:)), name: UIApplication.didEnterBackgroundNotification, object: nil)//When app goes background/ends saves data
         
         
         self.editingStyle = UITableViewCell.EditingStyle.none//Look up style
         print("%Editing style  None")
+        
+        
         tableView.reloadData()
         
         
             
     }//end view did load
+    
     @objc func appEnterBackGround(notification: NSNotification){
            print("%App passed to background")
+            //update map
+            model.producers.forEach{ model.producersNamed.updateValue($0, forKey: $0.nameProducer)}
+            
+        
            if !model.writeProducersInfosToDocuments(model.NAME_OF_PRODUCERS_FILE_IN_DOCUMENTS, folder: model.NAME_OF_FOLDER_IN_DOCUMENTS){
                notifyUser(self, alertTitle: "IO Error", alertMessage: "Error When Writting Producers", runOnOK: {_ in})
                
@@ -43,7 +75,8 @@ class ViewController: UITableViewController {
        }
     
     @IBAction func returnfromDetail(segue:UIStoryboardSegue)->Void{//Method for returning from Segues
-           model.producers.forEach{ b in b.beersCollect?.sort(by: {($0.nameBeer) < ($1.nameBeer)})}
+           //model.producers.forEach{ b in b.beersCollect?.sort(by: {($0.nameBeer) < ($1.nameBeer)})}
+            model.producers.forEach{ b in b.beersCollect?.sort(by: {($0.nameBeer) < ($1.nameBeer)})}
            tableView.reloadData()
        }
        
@@ -51,6 +84,9 @@ class ViewController: UITableViewController {
        //Functions for buttons
        @IBAction func addBeerAct(_ sender: Any){
            print("             #Vamos a añadir una cerveza")
+        //model.producersNamed["Cervezas Segovia S.L"]!.beersCollect?.forEach{print("                        BEERS IN NAMED --> \($0.nameBeer)")}
+        model.producersNamed.forEach{print("                        BEERS IN NAMED --> \($0.value.beersCollect!.forEach{print("                        BEERS IN NAMED --> \($0.nameBeer)")})")}
+        
            if self.editingStyle == UITableViewCell.EditingStyle.none{
                print("             #Estilo de edit activado")
                print("%Number producers --> \(model.producers.count)")
@@ -62,7 +98,7 @@ class ViewController: UITableViewController {
            }else if self.editingStyle == .insert{
                    
                    self.editingStyle = UITableViewCell.EditingStyle.none
-           
+
                    delBeerButt.isEnabled = true
                    addBeerButt.setTitle("Add Beer", for: .normal)//¿?¿ Posible error
                    tableView.setEditing(false, animated: true)
@@ -70,7 +106,6 @@ class ViewController: UITableViewController {
                }
            
        }
-       
        
        @IBAction func delBeerAct(_ sender: Any){
            
@@ -136,17 +171,22 @@ class ViewController: UITableViewController {
     //
     func numSect(in tableView: UITableView) -> Int{
         print("%Number of sections --> \(model.producers.count)")
-        return model.producers.count
+       return model.producers.count
+      
     }
     //TableViewCell
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let producerNumber = section
+        
         let producer = model.producers[producerNumber]
+        
         return producer.beersCollect?.count ?? 0
     }
     
    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let producerNum = indexPath.section
+       // let producerName = model.producers[producerNum].nameProducer
+        //let producer = model.producersNamed[producerName]
         let producer = model.producers[producerNum]
         let b = producer.beersCollect?[indexPath.row]
         
@@ -229,11 +269,20 @@ class ViewController: UITableViewController {
            performSegue(withIdentifier: "segueToBeer", sender: beer)
            
        }
+    
     var selectedBeer: Beer?
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath){
         let producer = model.producers[indexPath.section]
+        let producerNum = indexPath.section
+        
+       // let producerName = model.producers[producerNum].nameProducer
+       // let producer = model.producersNamed[producerName]
+        
+        
         let beerLine =  producer.beersCollect?[indexPath.row]
+        
+        
         selectedBeer = beerLine
         performSegue(withIdentifier: "segueToBeer", sender: nil)
         
@@ -267,3 +316,96 @@ class ViewController: UITableViewController {
        }
 }
 
+  /*
+ 
+ 
+ 
+ 
+ 
+ //=================================================================================
+
+ var uniqueValues = Set<String>()
+ var resultDict = [String: Producer]()
+ print(self.producersNamed.count)
+ resultDict = self.producersNamed
+ self.producers.removeAll()
+ var allBears:[Beer] = []
+
+ 
+ self.producersNamed.forEach {(index,value) in
+     
+     allBears+=value.beersCollect!
+     
+ }
+  allBears = allBears.sorted(by: { $0.nameBeer < $1.nameBeer})
+ var indexBeer = 2
+ var oldNameBeer = allBears[0].nameBeer
+ print("aaaaaaaaaaaaaaaaaa")
+ allBears.forEach{ print($0.nameBeer)}
+ print(allBears.count)
+ 
+ for (index, bear) in allBears.enumerated() {
+     if !(index==allBears.count-1){
+         
+         
+         var nameBeerCurrent = String(bear.nameBeer)
+         print("ssssssssssssss")
+         print(allBears[index].nameBeer)
+         var nameBeerNext = allBears[index+1].nameBeer
+         print(allBears[index+1].nameBeer)
+         nameBeerCurrent = allBears[index].nameBeer
+         print(nameBeerNext==nameBeerCurrent)
+         if (nameBeerNext.elementsEqual(nameBeerCurrent))
+         {
+             print("okkkkkk")
+             if (allBears[index].capBeer == allBears[index+1].capBeer && allBears[index].expDateBeer == allBears[index+1].expDateBeer && allBears[index].nationalityBeer == allBears[index+1].nationalityBeer && allBears[index].rateBeer == allBears[index+1].rateBeer){
+                 print("the same beer exist !!!!")
+                 let indexProd = self.producersNamed.index(forKey: allBears[index].nameBeer)
+                 
+                  self.producersNamed[indexProd!].value.beersCollect?.filter{
+                     $0.nameBeer == allBears[index].nameBeer
+                 }.first?.change(p_nameBeer:allBears[index].nameBeer+"_"+String(index))
+                 
+                 print("------------------")
+                 
+                 print(self.producersNamed[indexProd!].value.beersCollect?.filter{
+                     $0.nameBeer == allBears[index].nameBeer
+                 }.first?.nameBeer)
+
+                 if (oldNameBeer == allBears[index].nameBeer)
+                 {
+                     indexBeer = indexBeer+1
+                     oldNameBeer = allBears[index].nameBeer
+                     
+                 }else{
+                     indexBeer = 2
+                 }
+                 
+                 
+                 
+             }
+         }
+     }
+ }
+ 
+
+ resultDict = sortWithKeys(resultDict)
+ 
+ var numberIterator = resultDict.makeIterator()
+ print(resultDict.count)
+ while let num = numberIterator.next(){
+     print(self.producersNamed.count)
+     print(self.producersNamed.endIndex)
+     print("DUPLICATION")
+     print(num.key)
+     if (num.key == numberIterator.next()?.key)
+     {
+         print("-------------DUPLICATE PRODUCER --------------")
+         let index = self.producersNamed.index(forKey: num.key)
+         self.producersNamed = switchKey(self.producersNamed,fromKey: num.key, toKey: num.key + "_02")
+     }
+ }
+ 
+//=================================================================================
+*/
+ 
